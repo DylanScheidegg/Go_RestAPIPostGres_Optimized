6a36abf63acd1f1c37dbe76b57dbdbaf2aa4a841
Dylan Scheidegg <62270254+DylanScheidegg@users.noreply.github.com>
2021-04-12 20:53:06 -0400

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/sohamkamani/birdpedia
+
+go 1.14
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/lib/pq v1.10.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+// http://127.0.0.1:8080/assets/
+
+// The new router function creates the router and
+// returns it to us. We can now use this function
+// to instantiate and test the router outside of the main function
+func newRouter() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", handler).Methods("GET")
+
+	// Declare the static file directory and point it to the directory we just made
+	staticFileDirectory := http.Dir("./assets/")
+	// Declare the handler, that routes requests to their respective filename.
+	// The fileserver is wrapped in the `stripPrefix` method, because we want to
+	// remove the "/assets/" prefix when looking for files.
+	// For example, if we type "/assets/index.html" in our browser, the file server
+	// will look for only "index.html" inside the directory declared above.
+	// If we did not strip the prefix, the file server would look for "./assets/assets/index.html", and yield an error
+	staticFileHandler := http.StripPrefix("/assets/", http.FileServer(staticFileDirectory))
+	// The "PathPrefix" method acts as a matcher, and matches all routes starting
+	// with "/assets/", instead of the absolute route itself
+	r.PathPrefix("/assets/").Handler(staticFileHandler).Methods("GET")
+
+	r.HandleFunc("/bird", getBirdHandler).Methods("GET")
+	r.HandleFunc("/bird", createBirdHandler).Methods("POST")
+	return r
+}
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "PASSWORD HERE"
+	dbname   = "bird_enclopedia"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+
+	InitStore(&dbStore{db: db})
+
+	// The router is now formed by calling the `newRouter` constructor function
+	// that we defined above. The rest of the code stays the same
+	r := newRouter()
+	fmt.Println("Serving on port 8080")
+	http.ListenAndServe(":8080", r)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
